af21a41da9e020811d9d6d4e03453f6265a360d8
Raahim72 <raahimhussain80@gmail.com>
2024-10-02 18:19:25 +0500

    aayen

---
+++ tup.py
@@ -0,0 +1,12 @@
+my_tuple = ()
+print(my_tuple)
+
+my_tuple = (1,2,3)
+print(my_tuple)
+
+my_tuple = ("mouse",[8,4,6],(1,2,3))
+print(my_tuple)
+
+my_tuple = ("p","e","r","m","i","t")
+print(my_tuple[0])
+print(my_tuple[5])
